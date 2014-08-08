aa6673fc44bb408deaf886547d074dd9fd889815
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2014-08-08 10:00:22 +0200

    Upgrade micro-infra-spring to 0.0.3 final

--- src/main/groovy/com/ofg/microservice/config/WebAppConfiguration.groovy
+++ src/main/groovy/com/ofg/microservice/config/WebAppConfiguration.groovy
@@ -1,11 +1,11 @@
 package com.ofg.microservice.config
 
 import com.ofg.infrastructure.web.config.SwaggerConfiguration
-import com.ofg.infrastructure.web.config.WebConfiguration
+import com.ofg.infrastructure.web.config.WebInfrastructureConfiguration
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
 @Configuration
-@Import([WebConfiguration, SwaggerConfiguration])
+@Import([WebInfrastructureConfiguration, SwaggerConfiguration])
 class WebAppConfiguration {
 }
