5cecd9870ca41cd6eabd2f093fa7e688b8205afe
Kamil Szymanski <kamil.szymanski.dev@gmail.com>
2014-08-08 10:53:43 +0200

    register health check endpoint

--- src/main/groovy/com/ofg/microservice/config/WebApplicationConfiguration.groovy
+++ src/main/groovy/com/ofg/microservice/config/WebApplicationConfiguration.groovy
@@ -1,11 +1,11 @@
 package com.ofg.microservice.config
 
+import com.ofg.infrastructure.config.WebAppConfiguration
 import com.ofg.infrastructure.web.config.SwaggerConfiguration
-import com.ofg.infrastructure.web.config.WebInfrastructureConfiguration
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
 @Configuration
-@Import([WebInfrastructureConfiguration, SwaggerConfiguration])
-class WebAppConfiguration {
+@Import([WebAppConfiguration, SwaggerConfiguration])
+class WebApplicationConfiguration {
 }
